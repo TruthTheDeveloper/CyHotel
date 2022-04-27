4992c5243ad4fec999f4d7f7ac2000628ee2ab88
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-04-27 02:22:39 -0700

    succeeded in making the rooms component swiperable

--- src/components/RoomsCard.vue
+++ src/components/RoomsCard.vue
@@ -1,8 +1,15 @@
 <template>
-    <section>
-        <div>
-            <img/>
+        <div class="m-5 px-6 py-4 shadow-xl border mb-14">
+        <div class="w-64 h-72 bg-red-700">
+            <img src="../assets/images/room3.jpg" class="h-full"/>
         </div>
-        <div></div>
-    </section>
+        <div class="">
+            <h1 class="font-semibold text-xl my-3">Duplex Room</h1>
+            <p class="text-sm my-2 text-slate-500  w-44">Semi double bed 1 guest room 2 windows</p>
+            <p>Starting from from <span class="font-semibold text-xl"> $50/night</span></p>
+            <button class="bg-pink-500 my-5 px-6 py-3 text-white">
+                Book now
+            </button>
+        </div>
+    </div>
 </template>
--- src/page/components/Rooms.vue
+++ src/page/components/Rooms.vue
@@ -1,10 +1,60 @@
 <template>
-    <section>
-        <rooms-card/>
+    <section class="flex justify-center">
+        <swiper
+        :modules="modules"
+        :slides-per-view="4"
+        :space-between="50"
+        navigation
+        :pagination="{ clickable: true }"
+        :scrollbar="{ draggable: true }"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+        >
+        <swiper-slide><rooms-card/></swiper-slide>
+        <swiper-slide><rooms-card/></swiper-slide>
+        <swiper-slide><rooms-card/></swiper-slide>
+        <swiper-slide><rooms-card/></swiper-slide>
+        <swiper-slide><rooms-card/></swiper-slide>
+        <swiper-slide><rooms-card/></swiper-slide>
+    </swiper>
     </section>
 </template>
 
-<script setup>
+<script>
 import RoomsCard from '../../components/RoomsCard.vue';
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+      RoomsCard
+    },
+
+setup() {
+      const onSwiper = (swiper) => {
+        console.log(swiper);
+      };
+      const onSlideChange = () => {
+        console.log('slide change');
+      };
+      return {
+        onSwiper,
+        onSlideChange,
+        modules: [Navigation, Pagination, Scrollbar, A11y],
+      };
+    },
+}
+
 
 </script>
