2b74ad93dd274d51aab0246a33113e9789f43eee
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-04-29 04:59:24 -0700

    installed the v-calender

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+//Golbal css
 import './index.css';
-//vue-app/src/main.js
+
+// vue components
+import 'v-calendar/dist/style.css';
+
+// font awesome libary
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -12,6 +17,13 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 library.add(far);
 import { dom } from "@fortawesome/fontawesome-svg-core";
 dom.watch();
+
+
+// vue Calender
+import VCalendar from 'v-calendar';
+
+// Use plugin with defaults
+app.use(VCalendar, {})
 
 const app = createApp(App);
 
