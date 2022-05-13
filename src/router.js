b6124afedff68df260350629dd7522051670b455
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-05-13 04:03:54 -0700

    performed code spliting on the router.js

--- src/router.js
+++ src/router.js
@@ -1,16 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import LandingPage from './page/landingPage.vue';
-import BookingPage from './page/bookingPage.vue';
-import Register from './page/Auth/register.vue';
-import Login from './page/Auth/login.vue';
-import Dashboard from './page/DashBoard/dashboard.vue';
-import Reservation from './page/DashBoard/reservation.vue';
-import AdminLandingPage from './page/Admin/adminLandingPage.vue';
-import AdminAuth from './page/Admin/adminAuth.vue';
-import AdminDashboard from './page/Admin/adminDashboard.vue';
-import createRoomForm from './page/Admin/createRoomForm.vue';
-import UpdateRoomForm from './page/Admin/updateRoomform.vue';
-import Management from './page/Admin/management.vue';
+const LandingPage = () => import("./page/landingPage.vue");
+const BookingPage = () => import("./page/bookingPage.vue");
+const Register = () => import("./page/Auth/register.vue");
+const Login = () => import("./page/Auth/login.vue")
+const DashBoard = () => import("./page/Dashboard/dashboard.vue")
+const Reservation = () => import('./page/DashBoard/reservation.vue')
+const AdminLandingPage = () => import('./page/Admin/adminLandingPage.vue')
+const AdminAuth = () => import('./page/Admin/adminLandingPage.vue')
+const AdminDashboard  = () => import('./page/Admin/adminDashboard.vue')
+const createRoomForm = () => import('./page/Admin/createRoomForm.vue')
+const UpdateRoomForm = () => import('./page/Admin/updateRoomform.vue')
+const Management = () => import('./page/Admin/management.vue')
+// import LandingPage from './page/landingPage.vue';
+// import BookingPage from './page/bookingPage.vue';
+// import Register from './page/Auth/register.vue';
+// import Login from './page/Auth/login.vue';
+// import Dashboard from './page/DashBoard/dashboard.vue';
+// import Reservation from './page/DashBoard/reservation.vue';
+// import AdminLandingPage from './page/Admin/adminLandingPage.vue';
+// import AdminAuth from './page/Admin/adminAuth.vue';
+// import AdminDashboard from './page/Admin/adminDashboard.vue';
+// import createRoomForm from './page/Admin/createRoomForm.vue';
+// import UpdateRoomForm from './page/Admin/updateRoomform.vue';
+// import Management from './page/Admin/management.vue';
 
 export const router = createRouter({
     history:createWebHistory(),
@@ -38,7 +50,7 @@ export const router = createRouter({
         {
             name:'Dashboard',
             path:'/dashboard',
-            component:Dashboard
+            component:DashBoard
         },
         {
             name:'Reservation',
