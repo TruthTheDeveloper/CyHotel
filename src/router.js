a8023708c22004298c5756d6f8c1e0d1e7e985a7
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-05-13 04:33:51 -0700

    lowered case the first letter of page component

--- src/router.js
+++ src/router.js
@@ -1,28 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
-const LandingPage = () => import("./page/landingPage.vue");
-const BookingPage = () => import("./page/bookingPage.vue");
-const Register = () => import("./page/Auth/register.vue");
-const Login = () => import("./page/Auth/login.vue")
-const DashBoard = () => import("./page/Dashboard/dashboard.vue")
-const Reservation = () => import('./page/DashBoard/reservation.vue')
-const AdminLandingPage = () => import('./page/Admin/adminLandingPage.vue')
-const AdminAuth = () => import('./page/Admin/adminLandingPage.vue')
-const AdminDashboard  = () => import('./page/Admin/adminDashboard.vue')
-const createRoomForm = () => import('./page/Admin/createRoomForm.vue')
-const UpdateRoomForm = () => import('./page/Admin/updateRoomform.vue')
-const Management = () => import('./page/Admin/management.vue')
-// import LandingPage from './page/landingPage.vue';
-// import BookingPage from './page/bookingPage.vue';
-// import Register from './page/Auth/register.vue';
-// import Login from './page/Auth/login.vue';
-// import Dashboard from './page/DashBoard/dashboard.vue';
-// import Reservation from './page/DashBoard/reservation.vue';
-// import AdminLandingPage from './page/Admin/adminLandingPage.vue';
-// import AdminAuth from './page/Admin/adminAuth.vue';
-// import AdminDashboard from './page/Admin/adminDashboard.vue';
-// import createRoomForm from './page/Admin/createRoomForm.vue';
-// import UpdateRoomForm from './page/Admin/updateRoomform.vue';
-// import Management from './page/Admin/management.vue';
+// const LandingPage = () => import("./page/landingPage.vue");
+// const BookingPage = () => import("./page/bookingPage.vue");
+// const Register = () => import("./page/Auth/register.vue");
+// const Login = () => import("./page/Auth/login.vue")
+// const DashBoard = () => import("./page/Dashboard/dashboard.vue")
+// const Reservation = () => import('./page/DashBoard/reservation.vue')
+// const AdminLandingPage = () => import('./page/Admin/adminLandingPage.vue')
+// const AdminAuth = () => import('./page/Admin/adminLandingPage.vue')
+// const AdminDashboard  = () => import('./page/Admin/adminDashboard.vue')
+// const createRoomForm = () => import('./page/Admin/createRoomForm.vue')
+// const UpdateRoomForm = () => import('./page/Admin/updateRoomform.vue')
+// const Management = () => import('./page/Admin/management.vue')
+import landingPage from './page/landingPage.vue';
+import bookingPage from './page/bookingPage.vue';
+import register from './page/Auth/register.vue';
+import login from './page/Auth/login.vue';
+import dashBoard from './page/DashBoard/dashboard.vue';
+import reservation from './page/DashBoard/reservation.vue';
+import adminLandingPage from './page/Admin/adminLandingPage.vue';
+import adminAuth from './page/Admin/adminAuth.vue';
+import adminDashboard from './page/Admin/adminDashboard.vue';
+import createRoomForm from './page/Admin/createRoomForm.vue';
+import updateRoomForm from './page/Admin/updateRoomform.vue';
+import management from './page/Admin/management.vue';
 
 export const router = createRouter({
     history:createWebHistory(),
@@ -30,47 +30,47 @@ export const router = createRouter({
         {
             name:'LandingPage',
             path:'/',
-            component:LandingPage
+            component:landingPage
         },
         {
             name:'BookingPage',
             path:'/book',
-            component:BookingPage
+            component:bookingPage
         },
         {
             name:'Register',
             path:'/register',
-            component:Register
+            component:register
         },
         {
             name:'Login',
             path:'/login',
-            component:Login
+            component:login
         },
         {
             name:'Dashboard',
             path:'/dashboard',
-            component:DashBoard
+            component:dashBoard
         },
         {
             name:'Reservation',
             path:'/reservation',
-            component:Reservation
+            component:reservation
         },
         {
             name:'AdminPage',
             path:'/admin/landingpage',
-            component:AdminLandingPage
+            component:adminLandingPage
         },
         {
             name:'AdminAuth',
             path:'/admin/auth',
-            component:AdminAuth
+            component:adminAuth
         },
         {
             name:'adminDashboard',
             path:'/admin/dashboard',
-            component:AdminDashboard
+            component:adminDashboard
         },
         {
             name:'createRoomForm',
@@ -80,13 +80,13 @@ export const router = createRouter({
         {
             name:'updateRoomForm',
             path:'/admin/update/roomform',
-            component:UpdateRoomForm
+            component:updateRoomForm
         },
 
         {
             name:'management',
             path:'/admin/management',
-            component:Management
+            component:management
         },
 
     ]
