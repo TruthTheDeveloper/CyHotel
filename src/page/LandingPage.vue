16021bd9cddbbd6eafa886098c590028b75b92c3
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-05-13 03:18:06 -0700

    ran build

--- src/page/LandingPage.vue
+++ src/page/LandingPage.vue
@@ -56,7 +56,6 @@ import Credit from './components/Credit.vue';
 import Testimonial from './components/Testimonial.vue';
 import Explore from './components/Explore.vue';
 import Search from '../components/Search.vue';
-import BaseLayout from '../slot/BaseLayout.vue';
 
 
 // vue import
